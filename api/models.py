dafaaa4467266c9698d7aadd6875ed253fe43d46
César Mayta <cesarmayta@gmail.com>
2024-03-20 21:36:39 -0500

    models category and product

--- api/models.py
+++ api/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    
+    class Meta:
+        db_table = 'tbl_category'
+    
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    image = CloudinaryField('image',default='')
+    category = models.ForeignKey(Category,
+                                 related_name='products',
+                                 on_delete=models.RESTRICT)
+    
+    class Meta:
+        db_table = 'tbl_product'
+    
+    def __str__(self):
+        return self.name
