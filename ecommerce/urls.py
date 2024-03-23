d01aa8ca4657a0920d49e7309ca45f70d51a7ca6
César Mayta <cesarmayta@gmail.com>
2024-03-22 22:58:50 -0500

    swagger

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.urls import re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -23,9 +27,24 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView
 )
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="ECOMMERCE API",
+      default_version='v1',
+      description="Example of ecommerce API With Django and DRF",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="cesarmayta@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('api/',include('api.urls'))
 ]
