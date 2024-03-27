695a54e2b484f9064d0da9243baca0a5228408cf
César Mayta <cesarmayta@gmail.com>
2024-03-26 22:47:04 -0500

    configuracion

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -90,17 +90,10 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-"""DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}"""
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "ecommerce_g24",
+        "NAME": "db_ecommerce_g24",
         "USER": "postgres",
         "PASSWORD": "root",
         "HOST": "127.0.0.1",
